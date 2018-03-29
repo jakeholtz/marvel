585985a69150164f035156aeb6dc55a992aab529
Jake Holtz <jahholtz@gmail.com>
2018-03-29 15:07:37 -0700

    Implemented core functionality in JS/Vue

--- client/index.js
+++ client/index.js
@@ -1,6 +1,32 @@
 const app = new Vue({
   el: '#app',
   data: {
-    product: 'Product'
+    currQuestion: null,
+    currAnswers: [],
+    submitted: false,
+    finished: false,
+  },
+  methods: {
+    addQuestion: function(question) {
+      this.currQuestion = question;
+    },
+    addAnswer: function(answer) {
+      this.currAnswers.push(answer);
+      // app.$el.children[1].children[0].children[2].children[1].children[0].value = "Type answer here"
+    },
+    triggerSubmit: function() {
+      this.submitted = true;
+    },
+    triggerFinish: function() {
+      this.finished = true;
+    },
+    triggerReset: function() {
+      this.currQuestion = null;
+      this.currAnswers = [];
+      this.submitted = false;
+      this.finished = false;
+    },
   }
 })
+
+// function addQuestions(ques)
